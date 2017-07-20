4291ed21dbce38be16b4bf1b955c3f793b7571d4
Braden Sten <b.sten88@gmail.com>
2017-07-20 14:06:25 -0600

    integrated newgem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'devise', '~> 4.3'
 
 gem 'bootstrap', git: 'https://github.com/twbs/bootstrap-rubygem'
 
+gem 'sten_view_tool', git: 'https://github.com/braden-sten/sten_view_tool'
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end 
 	end
+
+  def copyright_generator
+    StenViewTool::Renderer.copyright 'Braden Sten', 'All right reserved'
+  end
 end
